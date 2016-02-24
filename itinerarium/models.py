42e4597b517cecdd9544ffa2ce567e18fd339e95
aretaon <julian.bender@posteo.de>
2016-02-24 09:56:18 +0000

    changed approved comments counter

--- itinerarium/models.py
+++ itinerarium/models.py
@@ -18,6 +18,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('itinerarium.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -31,3 +34,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
